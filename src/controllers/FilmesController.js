686528ed41a4679dba9b5f94c017db83362cb94f
Jessica <jb229507@gmail.com>
2024-01-25 21:02:31 -0300

    novosFilmes

--- src/controllers/FilmesController.js
+++ src/controllers/FilmesController.js
@@ -8,6 +8,13 @@ let listFilms = [
         ano: "2000",
         classificacao: "Livre",
         descricao: "O documentário busca mostrar as influências das telenovelas na construção da identidade étnica dos afro-brasileiros, então de discutir a incorporação positiva do negro na teledramaturgia."
+    },
+    {
+        id: 2,
+        nome: "Do que aprendi com minhas mais velhas",
+        ano: "2017",
+        classificacao: "Livre",
+        descricao: "Do que aprendi com minhas mais velhas é um documentário sobre a fé no Candomblé e como essa fé é transmitida de geração em geração. Um filme onde mulheres importantes no Candomblé da Bahia falam como aprenderam com seus mais velhos e como ensinam seus mais jovens. Um filme sobre tradição, amor e religiosidade."
     }
 ]
 
--- src/routes/FilmesRoute.js
+++ src/routes/FilmesRoute.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { list, add } from "../controllers/FilmesController.js";
 const filmesRouter = Router();
+const listFilms = list();
 
 filmesRouter.post('/add', (req, res) => {
     console.log('Dados recebidos: ', req.body)
@@ -9,9 +10,27 @@ filmesRouter.post('/add', (req, res) => {
     res.json({ newFilme })
 })
 
+
+
 filmesRouter.get('/list', (req, res) => {
-   const listFilms = list()
-   res.json({ listFilms })
+const nomes = []
+    for (let i = 0; i < listFilms.length; i++) {
+        nomes.push(listFilms[i].nome + ', id - ' + listFilms[i].id);
+    }
+    res.json({ nomes });
+});
+
+
+filmesRouter.get('/filme1', (req, res) => {
+    if (listFilms[0].id == 1) {
+     res.json({ filme1: listFilms[0] });
+   }
+})
+
+filmesRouter.get('/filme2', (req, res) => {
+    if (listFilms[1].id == 2) {
+     res.json({ filme2: listFilms[1] });
+   }
 })
 
 export { filmesRouter };
