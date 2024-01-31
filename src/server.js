8b051996b569a2094b2b16569052f76aefc2cf26
Jessica <jb229507@gmail.com>
2024-01-30 21:33:15 -0300

    update-all-files

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { filmesRouter } from "./routes/FilmesRoute.js";
+import { moviesRouter } from "./routes/MoviesRouter.js";
 const app = express();
 const port = 3001;
 
 
 app.use(express.json());
-app.use(filmesRouter);
+app.use(moviesRouter);
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+    console.log("Server run🚀");
 });
